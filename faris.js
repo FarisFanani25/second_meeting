a9d3838a978e5edde9149fece200ca7d35818c8b
Faris Fanani <127376549+FarisFanani25@users.noreply.github.com>
2023-06-14 14:30:14 +0700

    Add files via upload

---
+++ faris.js
@@ -0,0 +1,9 @@
+function fullName(name, nim) {
+    const objectBaru = {
+        fullName : name,
+        nim,
+    }
+    console.log(objectBaru)
+}
+
+fullName ('FERNANDO DJAKA', 'V3922022')
---
+++ looping.js
@@ -0,0 +1,10 @@
+const dataArray = [1, 2, 3, "Fernando djaka", {
+    name: "FARIS FANANI",
+    class: 22
+}]
+console.log(dataArray.length)
+
+for (let i = 1; i < dataArray.length; i++) {
+    console.log("index ---- ", i)
+    console.log(dataArray[i])
+}
